bfb53b9324f230c6a57ee4ad32b29259f17e352f
btgolley001-ship-it <btgolley001@converse.edu>
2025-11-12 11:05:07 -0500

    Fulfills Direction 1. Direction 2 pending; working on POST request.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+/* When a GET request is received for the document root ("/"), and the request has no form data, 
+* your node.js server should serve a copy of hw5-work.html. */
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('formpage', { title: 'Homework 5' });
+});
+
+/* When a POST request is received for the document root ("/"), 
+* and the request does have form data, your node.js server should serve a page that has:
+*   - A table containing the form data submitted
+*   - The same form found in hw5-work.html, including its CSS data, but not including the "Homework 5" header */
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  console.log('req.body (POST): '+JSON.stringify(req.body));
+  showPage(req, res, next);
 });
 
 module.exports = router;

80b296781e048a3d78fb14af029aa8c838552152
btgolley001-ship-it <btgolley001@converse.edu>
2025-11-17 09:32:02 -0500

    Updated POST request. Direction 2B behavior works properly. Direction 2A is pending. Formdata is being saved via function; names and emails functionally saved, graduation years and colors pending (planning to manually edit the html from "grad-year" to "gradyear" because who puts a hyphen there, seriously?). Misunderstood extra credit prompt and started keeping local variables of lists of all form data ever received. On the bright side, this code DOES save data between POST requests in a session!... But not to any credit in the assignment. Welp. At least I understand how locals work a little better now.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('formpage', { title: 'Homework 5, CSC 305 Database Design, Fall 2025' });
+  console.log('req.query (GET): '+JSON.stringify(req.query));
+  req.body = req.query; // Now behaves very much like POST
+  res.render('formpage', {title: 'Form Page First View',
+                        formdata: req.body});
 });
 
 /* When a POST request is received for the document root ("/"), 
@@ -16,21 +19,111 @@ router.get('/', function(req, res, next) {
 
 /* POST home page. */
 router.post('/', function(req, res, next) {
-  console.log('Name: '+ req.body.nameblank);
-  console.log('Email: '+ req.body.email);
-  console.log('Graduation Year: '+ req.body.grad-year);
-  console.log('Likes Plum: '+ req.body.Plum);
-  console.log('Likes Tomato: '+ req.body.Tomato);
-  console.log('Likes Wheat: '+ req.body.Wheat);
-  console.log('Likes Chocolate: '+ req.body.Chocolate);
   console.log('req.body (POST): '+JSON.stringify(req.body));
-  if (req.body.formpage) {
-    console.log('Form Page')
-    res.render('formpage', { title: '' });
+  setTitle(req, res, next);
+});
+
+/*
+ * Set req.app.locals.title to the desired title of the page, depending on
+ * whether HR_page is checked or not.  Call renderPage next.
+ * Note: would call listNames next if I had time to implement smth for extra credit.
+ */
+function setTitle(req, res, next) {
+  if (req.body.nameblank) {
+    req.app.locals.title = 'Form Page';
   }
   else {
-    res.render('formpage', { title: 'Homework 5, CSC 305 Database Design, Fall 2025'});
+    req.app.locals.title = 'Form Page First View';
   }
-});
+  listNames(req, res, next);
+}
+
+/*
+ * Set req.app.locals.new_name to req.body.nameblank
+ * Update req.app.locals.list_names with new_name
+ * Call listEmails next.
+ */
+function listNames(req, res, next) {
+  console.log('\nPOST is running listNames() ... req.app.locals.list_names: '+(req.app.locals.list_names));
+  // have an input for nameblank
+  if (req.body.nameblank != undefined) {
+    req.app.locals.new_name = req.body.nameblank;
+
+    // Been through POST before, just add newname onto list_names
+    if (req.app.locals.list_names) {
+      console.log('   Trying to add to req.app.locals.list_names with: '+(req.app.locals.new_name));
+      req.app.locals.list_names += ' / '+req.app.locals.new_name;
+    }
+
+    // First pass at POST, initialize variable 'list_names' with first instance
+    else {
+      console.log('   Trying to initialize req.app.locals.list_names with: '+(req.app.locals.new_name));
+      req.app.locals.list_names = req.app.locals.new_name;
+    }
+    console.log('   req.app.locals.list_names (POST): '+(req.app.locals.list_names));
+  }
+  // no input for nameblank
+  else {
+    req.app.locals.list_names = undefined;
+  }
+  console.log('POST has run listNames() ... list_names: '+(req.app.locals.list_names));
+  listEmails(req, res, next);
+}
+
+/* function listEmails
+ * Set req.app.locals.new_email to req.body.email
+ * Update req.app.locals.list_emails with new_email
+ * Call listGradYr next.
+ */
+function listEmails(req, res, next) {
+  console.log('\nPOST is running listEmails() ... req.app.locals.list_emails: '+(req.app.locals.list_emails));
+  // have an input for email
+  if (req.body.email != undefined) {
+    req.app.locals.new_email = req.body.email;
+
+    // Been through POST before, just add new_email onto list_names
+    if (req.app.locals.list_emails) {
+      console.log('   Trying to add to req.app.locals.list_emails with: '+(req.app.locals.new_email));
+      req.app.locals.list_emails += ' / '+req.app.locals.new_email;
+    }
+    
+    // First pass at POST, initialize variable 'list_emails' with first instance
+    else {
+      console.log('   Trying to initialize req.app.locals.list_emails with: '+(req.app.locals.new_email));
+      req.app.locals.list_emails = req.app.locals.new_email;
+    }
+    console.log('   req.app.locals.list_emails (POST): '+(req.app.locals.list_emails));
+  }
+  // no input for email
+  else {
+    req.app.locals.list_emails = undefined;
+  }
+  console.log('POST has run listEmails() ... list_emails: '+(req.app.locals.list_emails));
+  renderPage(req, res, next); // calling renderPage early to test if this works like i want it to
+}
+
+/* function listGradYr
+ * Set req.app.locals.new_gradyr to req.body.grad-year
+ * Update req.app.locals.list_gradyr with new_gradyr
+ * Call listColors next.
+ */
+
+/* function listColors
+ * Set req.app.locals.listgradyr to ???
+ * Call renderPage next.
+ */
+
+/*
+ * Marshal all the data that has been stashed in req.app.locals, and call res.render on formpage.
+ */
+function renderPage(req, res, next) {
+  res.render('formpage', { title: req.app.locals.title,
+                        formdata: req.body,
+                        new_name: req.app.locals.new_name,
+                        list_names: req.app.locals.list_names,
+                        new_email: req.app.locals.new_email,
+                        list_emails: req.app.locals.list_emails
+  });
+}
 
 module.exports = router;
